b52cc63d1e2b478f549d25a381521c64dd8c108a
Andrew Wu <andrewwu@66.net>
2017-05-19 16:35:06 +0800

    add

---
+++ BetTransactions.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VWParty.Infra.Messaging.BetTransactions;
+
+namespace BetTransactions.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BetMessageBus bus = new BetMessageBus();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                bus.PublishMessage("letou", new BetTransactionMessage()
+                {
+                    Id = Guid.NewGuid().ToString()
+                });
+                if (i % 100 == 0) Console.WriteLine("Message Published: {0}...", i);
+            }
+        }
+    }
+}
